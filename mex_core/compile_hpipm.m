85dde01474e21288498678cabb691bdf44aca1ca
Yutao <yutao@dei.unipd.it>
2018-05-03 17:16:51 +0200

    fix compiling hpipm in WINDOWS

--- mex_core/compile_hpipm.m
+++ mex_core/compile_hpipm.m
@@ -1,53 +1,69 @@
 
+%% detect OS type
+
+OS_MAC = 0;
+OS_LINUX = 0;
+OS_WIN = 0;
+
+if ismac
+    OS_MAC = 1;
+elseif isunix
+    OS_LINUX = 1;
+elseif ispc
+    OS_WIN = 1;
+else
+    disp('Platform not supported')
+end
+
+PREFIX = '';
+if OS_WIN
+    PREFIX = 'C:\msys';
+end
 %% hpipm sparse
 
-mexfiles_sp = ['hpipm_sparse.c ', ...
-            ' /opt/hpipm/lib/libhpipm.a ', ...
-            ' /opt/blasfeo/lib/libblasfeo.a ',...
-           ];
-
-%% hpipm dense
-
-mexfiles_d = ['hpipm_dense.c ', ...
-            ' /opt/hpipm/lib/libhpipm.a ', ...
-            ' /opt/blasfeo/lib/libblasfeo.a ',...
-           ];
-       
-%% hpipm condense
-mexfiles_cond = ['condensing_hpipm.c ', ...
-            ' /opt/hpipm/lib/libhpipm.a ', ...
-            ' /opt/blasfeo/lib/libblasfeo.a ',...
-           ];
-       
-%% blasfeo condense
-mexfiles_bcd = ['condensing_blasfeo.c ', ...
-            ' /opt/blasfeo/lib/libblasfeo.a ',...
-           ];
+if OS_LINUX
+    mexfiles_sp = ['hpipm_sparse.c ', ...
+                ' /opt/hpipm/lib/libhpipm.a ', ...
+                ' /opt/blasfeo/lib/libblasfeo.a ',...
+                   ];
+elseif OS_WIN
+    mexfiles_sp = ['hpipm_sparse.c ', ...
+                   PREFIX,'\opt\hpipm\lib\libhpipm.a ', ...
+                   PREFIX,'\opt\blasfeo\lib\libblasfeo.a ',...
+                   ];
+end
+                   
        
 %% hpipm pcond
-mexfiles_pcond = ['hpipm_pcond.c ', ...
-            ' /opt/hpipm/lib/libhpipm.a ', ...
-            ' /opt/blasfeo/lib/libblasfeo.a ',...
-           ];
+
+if OS_LINUX
+    mexfiles_pcond = ['hpipm_pcond.c ', ...
+                ' /opt/hpipm/lib/libhpipm.a ', ...
+                ' /opt/blasfeo/lib/libblasfeo.a ',...
+                   ];
+elseif OS_WIN
+    mexfiles_pcond = ['hpipm_pcond.c ', ...
+                   PREFIX,'\opt\hpipm\lib\libhpipm.a ', ...
+                   PREFIX,'\opt\blasfeo\lib\libblasfeo.a ',...
+                   ];
+end
 
 %%
        
 mexcmd = 'mex';
 
-mexcmd = [mexcmd, ' -O -DINT64 CFLAGS="\$CFLAGS -std=c99" GCC="/usr/bin/gcc-4.9"'];
-
-mexcmd = [mexcmd, ' -I.. -I/opt/hpipm/include -I/opt/blasfeo/include'];
+if OS_LINUX
+    mexcmd = [mexcmd, ' -O -DINT64 CFLAGS="\$CFLAGS -std=c99" GCC="/usr/bin/gcc-4.9"'];
+    mexcmd = [mexcmd, ' -I.. -I/opt/hpipm/include -I/opt/blasfeo/include'];
+elseif OS_WIN
+    mexcmd = [mexcmd, ' -O -DINT64 CFLAGS="$CFLAGS -std=c99" '];
+    mexcmd = [mexcmd, ' -I.. -I' PREFIX '\opt\hpipm\include -I' PREFIX '\opt\blasfeo\include'];
+end
 
 %%
 
 mexcmd_sp = [mexcmd, ' ', mexfiles_sp];
-mexcmd_d = [mexcmd, ' ', mexfiles_d];
-mexcmd_cond = [mexcmd, ' ', mexfiles_cond];
-mexcmd_bcd = [mexcmd, ' ', mexfiles_bcd];
 mexcmd_pcond = [mexcmd, ' ', mexfiles_pcond];
 
 eval(mexcmd_sp);
-% eval(mexcmd_d);
-% eval(mexcmd_cond);
-% eval(mexcmd_bcd);
 eval(mexcmd_pcond);
